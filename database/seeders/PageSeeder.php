fc7a08c1dc590279c4dd9f1e5ffc814a55c3a6e1
anslava00 <anslava00@mail.ru>
2022-03-31 22:36:02 +1000

    Lesson 2: Add pages (web)

---
+++ database/seeders/PageSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Page;
+use Illuminate\Database\Seeder;
+
+class PageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Page::query()->delete();
+        Page::factory(random_int(20, 30))->create();
+    }
+}
