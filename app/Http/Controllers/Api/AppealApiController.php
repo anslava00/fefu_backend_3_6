f85809784776f2314cda617555efb10a46ddb2af
anslava00 <anslava00@mail.ru>
2022-04-12 17:07:54 +1000

    Lesson 4: changed by comments

--- app/Http/Controllers/Api/AppealApiController.php
+++ app/Http/Controllers/Api/AppealApiController.php
@@ -5,31 +5,31 @@ use Vyuldashev\LaravelOpenApi\Attributes as OpenApi;
 
 use App\Models\Appeal;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\AppealFormRequest;
 use App\OpenApi\Parameters\AppealSubmitParameters;
 use App\OpenApi\Responses\EmptyResponse;
 use App\OpenApi\Responses\ErrorValidationResponse;
 use App\Sanitizers\PhoneSanitizer;
+use App\Http\Requests\AppealApiRequest;
 
 #[OpenApi\PathItem]
 class AppealApiController extends Controller
 {
     /**
-     * Display the specified resource.
-     * @param AppealFormRequest
+     * Accepts post requests and writes to the DB .
+     * @param AppealApiRequest
      * @return JsonResponse
      */
     #[OpenApi\Operation(tags: ["appeal"], method:'POST')]
     #[OpenApi\Parameters(factory: AppealSubmitParameters::class)]
     #[OpenApi\Response(factory: EmptyResponse::class, statusCode: 200)]
     #[OpenApi\Response(factory: ErrorValidationResponse::class, statusCode: 422)]
-    public function store(AppealFormRequest $request){
+    public function store(AppealApiRequest $request){
         $data = $request->validated();
 
         $appeal = new Appeal();
         $appeal->name = $data['name'];
-        $appeal->phone = PhoneSanitizer::sanitize($data['phone']);
-        $appeal->email = $data['email'];
+        $appeal->phone = PhoneSanitizer::sanitize($data['phone']??null);
+        $appeal->email = $data['email']??null;
         $appeal->message = $data['message'];
         
         $appeal->save();
