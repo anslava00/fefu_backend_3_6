529c9d73dac0a9bb742b8cf7ebc81fde77986ccc
anslava00 <anslava00@mail.ru>
2022-04-14 18:56:12 +1000

    Lesson 5: Add auth web

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Web\AppealWebController;
+use App\Http\Controllers\Web\AuthController;
 use App\Http\Controllers\Web\PageWebController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\ProfileController;
 use App\Http\Controllers\Web\NewsWebController;
+use Illuminate\Support\Facades\Route;
 
 
 /*
@@ -23,6 +25,18 @@ Route::get('/', function () {
 
 Route::get('/appeal', [AppealWebController::class, 'form'])->name('appeal.form');
 Route::post('/appeal', [AppealWebController::class, 'send'])->name('appeal.send');
+
+Route::get('/profile', action:[ProfileController::class, 'show'])
+    ->name('profile')
+    ->middleware('auth');
+
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
 
 Route::get('/pages', [PageWebController::class, 'index']);
 Route::get('/pages/{slug}', [PageWebController::class, 'show']);
